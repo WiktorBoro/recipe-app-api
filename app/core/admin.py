4660b2a5fd83986f379d0843275f22a1a950ec9c
WiktorBoro <105320026+WiktorBoro@users.noreply.github.com>
2022-10-17 11:54:24 +0200

    Added model test and models, added admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin  # noqa
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(User, UserAdmin)
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,43 @@
+"""
+Tests for models.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models"""
+    def test_create_user_with_email(self):
+        """Test creating a user with an email is success"""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_email = [
+            ['test12e421@email.com', 'test12e421@email.com'],
+            ['goop@Email.com', 'goop@email.com'],
+            ['TEST@EMAIL.com', 'TEST@email.com'],
+            ['Test22@EmaiL.com', 'Test22@email.com']
+        ]
+
+        for email, expected in sample_email:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_emial(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'testpass123')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@email.com',
+            'password123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
