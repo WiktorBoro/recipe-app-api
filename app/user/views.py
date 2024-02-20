984c77507698f0c6667cdf98917ab132a749ec80
Wiktor Borowicz <105320026+WiktorBoro@users.noreply.github.com>
2024-02-20 21:24:03 +0100

    Update views.py

--- app/user/views.py
+++ app/user/views.py
@@ -20,4 +20,5 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
     authentication_classes = [authentication.TokenAuthentication]
 
     def get_object(self):
+        # och no, changes!
         return self.request.user
